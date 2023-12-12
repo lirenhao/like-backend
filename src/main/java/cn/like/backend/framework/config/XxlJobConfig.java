b8a571cebfd784c355ff68ef088c99cf69424515
lirenhao <renhao.li@bjyada.com>
2023-12-12 08:55:05 +0800

    添加xxljob

---
+++ src/main/java/cn/like/backend/framework/config/XxlJobConfig.java
@@ -0,0 +1,45 @@
+package cn.like.backend.framework.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * xxlJob配置
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "xxl.job", name = "enabled", havingValue = "true")
+public class XxlJobConfig {
+
+    @Value("${xxl.job.admin.addresses:}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.accessToken:}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.appname:}")
+    private String appname;
+
+    @Value("${xxl.job.executor.address:}")
+    private String address;
+
+    @Value("${xxl.job.executor.ip:}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port:9999}")
+    private int port;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setAddress(address);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        return xxlJobSpringExecutor;
+    }
+}
